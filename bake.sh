efdfed1203bd0e949529fa5ef562f415ac250e40
Tyler Walker <tylerw.walker@hunterstrategy.net>
2021-03-09 06:20:34 -0800

    troubleshooting pupserver

--- bake.sh
+++ bake.sh
@@ -25,14 +25,24 @@ function lilAdmin () {
   docker exec -w /tmp/lilship lilship-k3d-webmux $@
 }
 
+function getPodName () {
+	lilKube kubectl get pods --namespace default -l "app.kubernetes.io/name=$1,app.kubernetes.io/instance=$1" -o jsonpath="{.items[0].metadata.name}"
+}
+
 function getLilShip () {
   #docker run --network host -w /tmp/lilship --rm -v /tmp/lilship:/tmp/lilship --name lilgitserver -d -p 2222:22 arvindr226/alpine-ssh
   lilGit clone https://github.com/brokorus/lilship.git
   lilGit clone https://github.com/brokorus/demo-control-repo.git
   docker exec -w /tmp/lilship lilship-k3d-webmux ssh-keygen -f /tmp/lilship/id_rsa -t rsa -N ''
-  lilKube kubectl apply -f /tmp/lilship/lilship/k8s/lilgit.yaml
-  lilKube kubectl create secret generic gitssh --from-literal=id_rsa="$(lilKube kubectl exec lilgitserver cat /tmp/lilship/id_rsa  | base64)" --from-literal=known_hosts="$(lilKube kubectl exec lilgitserver ssh-keyscan -p 22 -H lilgitserver | base64)" --from-literal=authorized_keys="$(lilAdmin cat /tmp/lilship/id_rsa.pub)"
-  lilKube exec lilgitserver cp /tmp/lilship/id_rsa.pub /home/git/.ssh/authorized_keys
+  lilKube kubectl apply -f /tmp/lilship/lilship/k8s/lilgit.yaml -w
+  PODNAME="$(getPodName lilgitserver)"
+  ID_RSA="$(lilKube kubectl exec $PODNAME cat /tmp/lilship/id_rsa  | base64)"
+  KNOWN_HOSTS="$(lilKube kubectl exec $PODNAME ssh-keyscan -p 22 -H lilgitserver | base64)"
+  AUTHORISED_KEYS="$(lilAdmin cat /tmp/lilship/id_rsa.pub)"
+
+  lilKube kubectl create secret generic gitssh --from-literal=id_rsa=$ID_RSA --from-literal=known_hosts=$KNOWN_HOSTS --from-literal=authorized_keys=$AUTHORISED_KEYS
+  lilKube exec $PODNAME cp /tmp/lilship/id_rsa.pub /home/git/.ssh/authorized_keys
+  lilKube exec $PODNAME chown -R git:git /tmp/lilship/id_rsa.pub 
 }
 
 function lilGit  () {
@@ -41,9 +51,9 @@ function lilGit  () {
 
 function localizeGit () {
   lilAdmin rm -rf /tmp/lilship/demo-control-repo/.git*
+  lilGit init
   lilGit config --global user.email "lilship@lilship.com"
   lilGit config --global user.name "Little Ship"
-  lilGit init
   lilGit add -A
   lilGit commit -m 'Initial'
   lilGit remote add origin ssh://root@lilgitserver:22/tmp/lilship/demo-control-repo.git
@@ -51,7 +61,7 @@ function localizeGit () {
 
 function createCluster () {
   lilAdmin k3d registry create lilshiplocalregistry.localhost --port 8083 
-  lilAdmin k3d cluster create -p "8089:80@loadbalancer" --agents 2 --volume /tmp/lilship:/tmp/lilship --api-port 8082 lilship --kubeconfig-update-default=true --registry-use k3d-lilshiplocalregistry.localhost:8083 --verbose
+  lilAdmin k3d cluster create -p "8089:80@loadbalancer" --agents 2 --volume /tmp/lilship:/tmp/lilship --api-port 8082 lilship --kubeconfig-update-default=true --registry-use k3d-lilshiplocalregistry.localhost:8083 
 }
 
 function lilKube () {
