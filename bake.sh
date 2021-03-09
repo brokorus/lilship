3a44b537b5a126354b4e816d3e7aac25a2f54419
Tyler Walker <tylerw.walker@hunterstrategy.net>
2021-03-09 10:20:48 -0800

    removed puppetboard

--- bake.sh
+++ bake.sh
@@ -18,7 +18,7 @@ function main () {
 function init () {
   rm -rf /tmp/lilship
   mkdir -p /tmp/lilship
-  docker run --network host -w /tmp/lilship -e KUBECONFIG=/tmp/lilship/kubeconfig -d -p 8081:8080 --name lilship-k3d-webmux -it -v /tmp/lilship:/tmp/lilship -v /var/run/docker.sock:/var/run/docker.sock --privileged  brokorus/lilship:1.1-k3dind-webmux
+  docker run -w /tmp/lilship -e KUBECONFIG=/tmp/lilship/kubeconfig -d -p 8081:8080 --name lilship-k3d-webmux -it -v /tmp/lilship:/tmp/lilship -v /var/run/docker.sock:/var/run/docker.sock --privileged  brokorus/lilship:1.1-k3dind-webmux
 }
 
 function lilAdmin () {
@@ -31,18 +31,20 @@ function getPodName () {
 
 function getLilShip () {
   #docker run --network host -w /tmp/lilship --rm -v /tmp/lilship:/tmp/lilship --name lilgitserver -d -p 2222:22 arvindr226/alpine-ssh
-  lilAdmin clone https://github.com/brokorus/lilship.git
-  lilAdmin clone https://github.com/brokorus/demo-control-repo.git
+  lilAdmin git clone https://github.com/brokorus/lilship.git
+  lilAdmin git clone https://github.com/brokorus/demo-control-repo.git
   docker exec -w /tmp/lilship lilship-k3d-webmux ssh-keygen -f /tmp/lilship/id_rsa -t rsa -N ''
-  lilKube kubectl apply -f /tmp/lilship/lilship/k8s/lilgit.yaml -w
   lilAdmin ssh-keyscan -p 22 -H lilgitserver > /tmp/lilship/known_hosts
   lilAdmin adduser git -D
   lilAdmin chown -R git:git /tmp/lilship
   lilKube kubectl create secret generic gitssh --from-file=id_rsa=/tmp/lilship/id_rsa --from-file=known_hosts=/tmp/lilship/known_hosts --from-file=authorized_keys=/tmp/lilship/id_rsa.pub
+  lilKube kubectl apply -f /tmp/lilship/lilship/k8s/lilgit.yaml 
+  lilAdmin ssh-keyscan -p 22 -H lilgitserver > /tmp/lilship/known_hosts
+  lilKube kubectl create secret generic gitssh --from-file=id_rsa=/tmp/lilship/id_rsa --from-file=known_hosts=/tmp/lilship/known_hosts --from-file=authorized_keys=/tmp/lilship/id_rsa.pub
 }
 
 function lilGit  () {
-  lilAdmin docker exec -w /tmp/lilship/lilship lilship-k3d-webmux git $@
+  lilAdmin docker exec -w /tmp/lilship/demo-control-repo lilship-k3d-webmux git $@
 }
 
 function localizeGit () {
@@ -57,7 +59,7 @@ function localizeGit () {
 
 function createCluster () {
   lilAdmin k3d registry create lilshiplocalregistry.localhost --port 8083 
-  lilAdmin k3d cluster create -p "8089:80@loadbalancer" --agents 2 --volume /tmp/lilship:/tmp/lilship --api-port 8082 lilship --kubeconfig-update-default=true --registry-use k3d-lilshiplocalregistry.localhost:8083 
+  lilAdmin k3d cluster create -p "8089:80@loadbalancer" --volume /tmp/lilship:/tmp/lilship --api-port 8082 lilship --kubeconfig-update-default=true --registry-use k3d-lilshiplocalregistry.localhost:8083 
 }
 
 function lilKube () {
